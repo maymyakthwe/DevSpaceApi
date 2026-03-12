acf95fe3835861186d9dd03f148acfc70efcd9a2
May Myak Thwe <thirimyat68@gmail.com>
2026-03-12 02:54:47 +0000

    added cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from app.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins (good for testing)
+    allow_credentials=True,
+    allow_methods=["*"],  # allow GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # allow custom headers
+)
 
 app.include_router(router)

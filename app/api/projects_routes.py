59d13326166f849ec66c619e29306daf303fe5bf
May Myak Thwe <thirimyat68@gmail.com>
2026-03-16 00:24:21 +0000

    project route async fix

--- app/api/projects_routes.py
+++ app/api/projects_routes.py
@@ -7,24 +7,26 @@ router = APIRouter(prefix="/projects", tags=["Download"])
 
 
 @router.post("/")
-def create_project(project: Project):
+async def create_project(project: Project):
     data = project.dict()
-    result = projects_collection.insert_one(data)
+    result = await projects_collection.insert_one(data)
     return {"_id": str(result.inserted_id)}
 
 
 @router.get("/")
-def get_projects():
+async def get_projects():
     projects = []
-    for p in projects_collection.find():
+
+    async for p in projects_collection.find():
         p["_id"] = str(p["_id"])
         projects.append(p)
+
     return projects
 
 
 @router.get("/{id}")
-def get_one(id: str):
-    project = projects_collection.find_one({"_id": ObjectId(id)})
+async def get_one(id: str):
+    project = await projects_collection.find_one({"_id": ObjectId(id)})
 
     if project is None:
         raise HTTPException(status_code=404, detail="project not found")
@@ -34,12 +36,14 @@ def get_one(id: str):
 
 
 @router.put("/{id}")
-def update_project(id: str, project: Project):
-    projects_collection.update_one({"_id": ObjectId(id)}, {"$set": project.dict()})
+async def update_project(id: str, project: Project):
+    await projects_collection.update_one(
+        {"_id": ObjectId(id)}, {"$set": project.dict()}
+    )
     return {"message": "updated"}
 
 
 @router.delete("/{id}")
-def delete_project(id: str):
-    projects_collection.delete_one({"_id": ObjectId(id)})
+async def delete_project(id: str):
+    await projects_collection.delete_one({"_id": ObjectId(id)})
     return {"message": "deleted"}
